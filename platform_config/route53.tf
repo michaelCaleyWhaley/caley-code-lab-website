3ea83c8882447759fe2f1018c058aeecd5fbcf19
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2023-09-21 21:49:41 +0100

    none working naked domain

--- platform_config/route53.tf
+++ platform_config/route53.tf
@@ -10,8 +10,8 @@ resource "aws_route53_record" "root_domain" {
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution_redirect.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution_redirect.hosted_zone_id
     evaluate_target_health = false
   }
 }
@@ -19,7 +19,7 @@ resource "aws_route53_record" "root_domain" {
 resource "aws_route53_record" "www_subdomain" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "www."
-  type    = "CNAME"
+  type    = "A"
 
   alias {
     name                   = aws_cloudfront_distribution.s3_distribution.domain_name
@@ -54,7 +54,7 @@ resource "aws_route53_record" "acm_tf_verification" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "_10b233803c909afa904a8873f361a6eb.caleycodelab.com."
   type    = "CNAME"
-  ttl     = "30"
+  ttl     = "300"
 
   records = [
     "_8712a92286b7a0fec4bcd218d16d2051.vkznmzfykm.acm-validations.aws.",
@@ -65,7 +65,7 @@ resource "aws_route53_record" "acm_tf_verification_www" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "_1e51e35946b5abf7b2bec9eba69d13aa.www.caleycodelab.com."
   type    = "CNAME"
-  ttl     = "30"
+  ttl     = "300"
 
   records = [
     "_dfe06fa49455a0a108a47fbcf516a93c.vkznmzfykm.acm-validations.aws.",
--- platform_config/staticWebsite.tf
+++ platform_config/staticWebsite.tf
@@ -48,3 +48,59 @@ data "aws_iam_policy_document" "s3_read_permissions" {
     resources = ["arn:aws:s3:::caley-code-lab-website/*"]
   }
 }
+
+
+##############################
+###### static website redirect
+##############################
+resource "aws_s3_bucket" "caley-code-lab-website-redirect" {
+  bucket = "caleycodelab.com"
+
+  tags = {
+    Name = "code lab website bucket-redirect"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "caley-code-lab-website-config-redirect" {
+  bucket = aws_s3_bucket.caley-code-lab-website-redirect.bucket
+
+
+  redirect_all_requests_to {
+    host_name = aws_s3_bucket_website_configuration.caley-code-lab-website-config.website_endpoint
+    protocol  = "https"
+  }
+}
+
+# # manually enable public access on AWS
+resource "aws_s3_bucket_public_access_block" "access_block_redirect" {
+  bucket = aws_s3_bucket.caley-code-lab-website-redirect.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "allow-public-access-redirect" {
+  bucket = aws_s3_bucket.caley-code-lab-website-redirect.bucket
+  policy = data.aws_iam_policy_document.s3_read_permissions_redirect.json
+}
+
+data "aws_iam_policy_document" "s3_read_permissions_redirect" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = ["arn:aws:s3:::caleycodelab.com/*"]
+  }
+}
+
+# output "instance_ip_addr" {
+#   value = aws_s3_bucket.caley-code-lab-website-redirect
+# }
